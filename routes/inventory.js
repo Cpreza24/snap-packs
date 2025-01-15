5ce4dd77c85bd315a20ef8264ee36d723a83fba8
Cesar Preza <cpreza93@yahoo.com>
2025-01-14 20:04:53 -0800

    removes unused files and replaces them with updated camera and lens directories

--- routes/inventory.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const inventoryCtrl = require('../controllers/inventory');
-
-router.get('/', inventoryCtrl.index);
-router.get('/camera/new', inventoryCtrl.newCamera);
-router.get('/lens/new', inventoryCtrl.newLens);
-
-module.exports = router;
